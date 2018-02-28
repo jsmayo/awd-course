5cd9e9ef8d26681b1edf3fe75c313ad16d20eb59
Steven Mayo <jsmayo91@gmail.com>
2018-02-27 19:55:08 -0500

    Updates constructors assigment   * Refactors Child object constructor to use Parent object constructor.

--- section-12/constructors.js
+++ section-12/constructors.js
@@ -2,7 +2,21 @@
 
 // Create a constructor function for a Person, each person should have a firstName, lastName, favoriteColor and favoriteNumber. Your function MUST be named Person.
 
+function Person(firstName, lastName, favColor, favNum) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favColor = favColor;
+  this.favNum = favNum;
+  this.multiplyFavoriteNumber = function(num) {
+    return num * this.favNum;
+  };
+}
+
 // Write a method called multiplyFavoriteNumber that takes in a number and returns the product of the number and the object created from the Person functions' favorite number.
+
+
+var me = new Person("Steven", "M", "Yellow", "16");
+console.log(me.firstName, me.lastName, me.favColor, me.favNum, me.multiplyFavoriteNumber(2));
 
 // PART 2
 
@@ -16,8 +30,9 @@ function Parent(firstName, lastName, favoriteColor, favoriteFood){
 }
 
 function Child(firstName, lastName, favoriteColor, favoriteFood){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.favoriteColor = favoriteColor;
-    this.favoriteFood = favoriteFood;
+    Parent.call(this, firstName, lastName, favoriteColor, favoriteFood);
 }
+
+var jr = new Child("firstname", "lastname", "favorite color", "favorite food");
+console.log(jr);
+console.log(jr.firstName, jr.lastName, jr.favoriteColor, jr.favoriteFood);
