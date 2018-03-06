342b9e92d10ab01038ca449d266e8f95dfa847d4
Steven <jsmayo91@gmail.com>
2018-03-06 00:40:24 +0000

    Updates comments in todos.js file

--- section-13/todo_api/routes/todos.js
+++ section-13/todo_api/routes/todos.js
@@ -1,9 +1,13 @@
+// will contain 5 routes 
+
 var express = require('express');
 // express.Router() allows routes to be broken in modular chunks
 var router = express.Router();
 
+//defining the /api/todos + '/' route. The /api/todos is prefixed in index.js
 router.get('/', function(req, res) {
     res.send("Hello from the TODO's route!");
 });
 
+//export routes for access outside of this file
 module.exports = router;
