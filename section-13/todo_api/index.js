2d58fecd601b03ec7a8bf4fe0dba3941a7060173
Steven <jsmayo91@gmail.com>
2018-03-07 10:01:37 +0000

    Corrects Mongoose error and adds POST route

--- section-13/todo_api/index.js
+++ section-13/todo_api/index.js
@@ -1,15 +1,23 @@
 // require express
 var express = require('express'),
-    // define var app to be express function as shortcut add.listen() ...
-    app = express(),
-    //c9 port for listening or localhost 
-    port = process.env.PORT; // || 3000;
+  // define var app to be express function as shortcut add.listen() ...
+  app = express(),
+  //c9 port for listening or localhost 
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser'); // need body-parser to access POST data
 
 // requiring the routes directory and have access to them due to exports!
 var todoRoutes = require("./routes/todos.js");
 
+// Tell app to use body-parser's json method and encoder method when handling data sent from POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
 app.get('/', function(req, res) {
-    res.send("Hello from the root route");
+  res.send("Hello from the root route");
 });
 
 /* making express prefix all routes with '/api/todos' formatting automatically
@@ -18,5 +26,5 @@ app.get('/', function(req, res) {
 app.use("/api/todos", todoRoutes);
 
 app.listen(process.env.PORT, function() {
-    console.log("App is listening on: " + port);
+  console.log("App is listening on: " + port);
 });
--- section-13/todo_api/routes/todos.js
+++ section-13/todo_api/routes/todos.js
@@ -8,17 +8,22 @@ var db = require("../models");
 
 //defining the /api/todos + '/' route. The /api/todos is prefixed in index.js
 router.get('/', function(req, res) {
-    // res.send("Hello from the TODO's route!");
-    // send all todo's
-    db.Todo.find() // find() with no params will find all entries
-        // using Promise syntax
-        .then(function(todos) {
-            res.json(todos);
-        })
-        .catch(function(err) {
-            res.send(err);
-        });
+  // res.send("Hello from the TODO's route!");
+  // send all todo's
+  db.Todo.find() // find() with no params will find all entries
+    // using Promise syntax
+    .then(function(todos) {
+      res.json(todos);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
 
+});
+
+router.post('/', function(req, res) {
+  //   response.send("this is the post route");
+  console.log(req.body);
 });
 
 //export routes for access outside of this file
